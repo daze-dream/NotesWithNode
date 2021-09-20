a0fed731c97a66a1eb1d4586da4f90c725f88180
CheetoDustNeo <misterrolypoly@gmail.com>
2021-09-20 00:37:58 -0500

    JSON challenge cleared

--- playground/1-json.js
+++ playground/1-json.js
@@ -9,7 +9,22 @@ const fs = require('fs')
 // const bookJSON = JSON.stringify(book)
 // fs.writeFileSync('./playground/1-json.json', bookJSON)
 
-const dataBuffer = fs.readFileSync('./playground/1-json.json')
-const dataJSON = dataBuffer.toString()
-const data = JSON.parse(dataJSON)
-console.log(data.title)
+// const dataBuffer = fs.readFileSync('./playground/1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+// Challenge coding
+// 1. load the data
+// const dataBuffer = fs.readFileSync('./playground/challenge_json.json')
+// const dataJSON2 = dataBuffer.toString()
+// data = JSON.parse(dataJSON2)
+// one liner edition
+const data = JSON.parse(fs.readFileSync('./playground/challenge_json.json').toString())
+// 2. change the contents of that data
+console.log(data);
+data.name = 'DUNA';
+data.age = 1099;
+console.log(data)
+// 3. overwrite old file or make a new one
+fs.writeFileSync('./playground/newjson.json', JSON.stringify(data))
